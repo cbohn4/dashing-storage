d1be11b81cf670f8b3dcd622b7f42f8569ac43b2
cbohn <cbohn4@unl.edu>
2019-02-28 15:02:12 -0600

    Fixed Python Path

--- storage_probe.sh
+++ storage_probe.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+source /util/opt/lmod/lmod/init/profile
 module load anaconda
 source activate dashing
-/usr/bin/python /util/admin/dashing-storage/storage_probe.py
+python /util/admin/dashing-storage/storage_probe.py
 source deactivate dashing
 module unload anaconda
