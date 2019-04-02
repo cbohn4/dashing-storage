6a07ade7337445a3c3eca549afb7da5b532e1461
cbohn <cbohn4@unl.edu>
2019-04-02 13:56:15 -0500

    Fixed Module Shenanigans

--- storage_probe.sh
+++ storage_probe.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source /util/opt/lmod/lmod/init/profile
+module use /util/opt/hcc-modules/Common
 module load anaconda
 source activate dashing
 python /util/admin/dashing-storage/storage_probe.py
