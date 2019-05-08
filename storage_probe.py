744b00f24e2a43d83c7233d8600cd364e117d778
cbohn <cbohn4@unl.edu>
2019-05-08 10:15:37 -0500

    Updated to match with newer dashboard schema

--- storage_probe.py
+++ storage_probe.py
@@ -44,14 +44,10 @@ def main():
         if len(split_line) < 5:
             continue
         if split_line[5] == MOUNT:
-            dash = dashing.DashingImport('viz.unl.edu', auth_token = auth_key)
-            dashUNO = dashing.DashingImport('viz.unl.edu',port=4000, auth_token = auth_key)
+            dash = dashing.DashingImport('viz.unl.edu', port=3030,auth_token = auth_key)            
             send_dict = { 'min': 0, 'max': float("%.1f" % (float(split_line[1]) / terabyte)) , 'value': float("%.1f" % (float(split_line[2]) / terabyte)), 'moreinfo': "Capacity: %s" % sizeof_fmt(int(split_line[1])) }
             dash.SendEvent(CLUSTER+'Storage', send_dict)
             dash.SendEvent('HCCAmazonPrice', {CLUSTER.lower()+'Storage': send_dict['value']})
-            
-            dashUNO.SendEvent(CLUSTER+'Storage', send_dict)
-            dashUNO.SendEvent('HCCAmazonPrice', {CLUSTER.lower()+'Storage': send_dict['value']})
 
 
     # Send the number of jobs running
@@ -71,7 +67,7 @@ def main():
                 per_user_cores[username] = 0
             per_user_cores[username] += int(cores)
         except:
-            print "Error parsing line: %s" % line
+            print("Error parsing line: " + line)
             pass
         
     if os.path.isfile('dashing.txt') and os.access('dashing.txt', os.R_OK):
@@ -79,7 +75,7 @@ def main():
         with open('dashing.txt', 'r') as file:
             last_running_cores = int(file.read())
     else:
-        print "Error reading from dashing.txt"
+        print("Error reading from dashing.txt")
         date = time.time() + 3600
         last_running_cores = sum_running_cores
     with open('dashing.txt', 'w') as file:
@@ -87,9 +83,6 @@ def main():
     date = time.strftime('%m-%d-%Y %H:%M:%S', time.localtime(date))
     dash.SendEvent(CLUSTER+'Running', {'current': sum_running_cores, 'last': last_running_cores, 'last_period': date})
     dash.SendEvent('HCCAmazonPrice', {CLUSTER+'Cores': sum_running_cores})
-    
-    dashUNO.SendEvent(CLUSTER+'Running', {'current': sum_running_cores, 'last': last_running_cores, 'last_period': date})
-    dashUNO.SendEvent('HCCAmazonPrice', {CLUSTER+'Cores': sum_running_cores})
     
     # send number of completed jobs
     current_time = time.strftime('%m/%d/%y-%H:%M:%S', time.localtime(time.time()))
@@ -100,7 +93,7 @@ def main():
     (stdout, stderr) = p.communicate()
     jobs_completed = len(stdout.split())
     path = '/common/swanson/.dashing/'
-    files = ['crane_jobs.txt', 'tusker_jobs.txt', 'sandhills_jobs.txt']
+    files = ['crane_jobs.txt', 'rhino_jobs.txt']
     filename = path + files[0]
     with open(filename, 'w') as file:
         file.write(str(jobs_completed))
@@ -111,11 +104,10 @@ def main():
             with open(filename, 'r') as file:
                 total_jobs += int(file.read())
         else:
-            print "Error reading from %s" % filename
+            print("Error reading from " + filename)
     
     dash.SendEvent('JobsCompleted', {'current': total_jobs})
     
-    dashUNO.SendEvent('JobsCompleted', {'current': total_jobs})
 
 
     #Send number of CPU Hours for Today
@@ -125,7 +117,7 @@ def main():
     (stdout, stderr) = p.communicate()
     stdout = map(int, stdout.split())
     hours_completed = sum(stdout)/3600
-    files = ['crane_hours.txt', 'tusker_hours.txt', 'sandhills_hours.txt']
+    files = ['crane_hours.txt', 'rhino_hours.txt']
     filename = path + files[0]
     with open(path + CLUSTER.lower()+'_hours.txt', 'w') as file:
         file.write(str(hours_completed))
@@ -136,35 +128,33 @@ def main():
             with open(filename, 'r') as file:
                 total_hours += int(file.read())
         else:
-            print "Error reading from %s" % filename
+            print("Error reading from " + filename)
 
 
     dash.SendEvent('HoursToday', {'current': total_hours})
     
-    dashUNO.SendEvent('HoursToday', {'current': total_hours})
     
     # Send Anvil Information
     ## Time Delay is to allow dashing to keep up with POST
     
     f = urllib2.urlopen("http://anvil-beta.unl.edu:8123/")
     rawData = json.load(f)
-    dashUNO.SendEvent('AnvilTile', {'current_vm': rawData["vm_count"]})
+    dash.SendEvent('AnvilTile', {'current_vm': rawData["vm_count"]})
     time.sleep(1)
-    dashUNO.SendEvent('AnvilTile', {'current_cores': rawData["core_count"]})
+    dash.SendEvent('AnvilTile', {'current_cores': rawData["core_count"]})
     time.sleep(1)
-    dashUNO.SendEvent('AnvilTile', {'current_mem': str(round(int(rawData["mem_count"])/(1024.0**2),2))})
+    dash.SendEvent('AnvilTile', {'current_mem': str(round(int(rawData["mem_count"])/(1024.0**2),2))})
     time.sleep(1)
-    dashUNO.SendEvent('AnvilTile', {'current_vol': str(round(int(rawData["volume_gb"])/1024.0,2))})
+    dash.SendEvent('AnvilTile', {'current_vol': str(round(int(rawData["volume_gb"])/1024.0,2))})
     time.sleep(1)
-    dashUNO.SendEvent('AnvilTile', {'current_disk': str(round(int(rawData["disk_gb"])/1024.0,2))})
+    dash.SendEvent('AnvilTile', {'current_disk': str(round(int(rawData["disk_gb"])/1024.0,2))})
 
     # Red Storage
     redT2 = urllib2.urlopen("http://t2.unl.edu:8088/dfshealth.jsp")
     redData = re.findall("\d+\.\d+",str(redT2.read()))
     dash.SendEvent('RedStorage', {'min': 0, 'max': float(redData[9])*1024, 'value': float(redData[10])*1024, 'Capacity': redData[9] + " PB"})  
-    dashUNO.SendEvent('RedStorage', {'min': 0, 'max': float(redData[9])*1024, 'value': float(redData[10])*1024, 'Capacity': redData[9] + " PB"}) 
     dash.SendEvent('HCCAmazonPrice', {'redStorage':float(redData[10])*1024})  
-    dashUNO.SendEvent('HCCAmazonPrice', {'redStorage':float(redData[10])*1024})
+    
     
     
     
@@ -191,7 +181,7 @@ def main():
     
     
     ## Pull top Users
-    files = ['crane_users.txt', 'tusker_users.txt']
+    files = ['crane_users.txt', 'rhino_users.txt']
     topUsers = {}
     for file in files:
         filename = path + file
